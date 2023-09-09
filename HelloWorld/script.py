0d9479562320d27f7d2b12831519edef1ed7b1db
Ismael <90379217+koto253@users.noreply.github.com>
2023-09-09 03:21:24 -0400

    Update script.py

--- HelloWorld/script.py
+++ HelloWorld/script.py
@@ -1,1 +1,14 @@
-print("hello world!")
+from datetime import datetime
+
+class Person:
+    def __initi__(self, name, dob, hair_color, nationality):
+        self.name = name
+        self.dob = dob
+        self.hair_color = hair_color
+        self.nationality = nationality
+
+    def printIdentity(self):
+        print(f"I'm {self.name}, I am born in {self.dob}, my nationality is {self.nationality}")
+
+identity = Person("Ismael", 1995, "black", "Guinean")
+identity.printIdentity()
