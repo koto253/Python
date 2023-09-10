c0061584d0cd9514cd17f62f45234a17929c3b48
Ismael <90379217+koto253@users.noreply.github.com>
2023-09-10 03:44:57 +0000

    python mock interview with chatgpt

---
+++ MockInterview/mock_interview_question_by_chatgpt.py
@@ -0,0 +1,25 @@
+# 1. Function
+# What is a Python Function?
+print("A reusable bloack of code that performs a specific task")
+
+#How do you define a function in python?
+print("Using the 'def' keyword")
+
+#What is the purpose of the 'return' statement in function?
+print("It specifies the value that the function should output")
+
+#Can a Python function have multiple return statements?
+print("Yes, and each return statement can return a different value.")
+
+#2. Lists
+#How to create an empty list in python?
+list = []
+list.append("hello world")
+list.extend("i love my new job", "i am grateful")
+print(list)
+
+# Explain list slicing and provide an example.
+print("List slicing is a way to select and extract a portion of a list")
+
+# How would you check if an element exists in a list?
+print("Use the in keyword")
